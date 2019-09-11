776ebcc85035645466a73621c9259142da3f5887
vjenacka <edins613@gmail.com>
2019-09-11 02:11:55 +0200

    added firebase config and set up redux-thunk for async actions

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { createProject } from "../../store/actions/projectActions";
+import { connect } from "react-redux";
 
 export class CreateProject extends Component {
   state = {
@@ -12,7 +14,7 @@ export class CreateProject extends Component {
   };
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.createProject(this.state)
   };
 
   render() {
@@ -26,10 +28,16 @@ export class CreateProject extends Component {
           </div>
           <div className="input-field">
             <label htmlFor="content">Project Content</label>
-            <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
+            <textarea
+              id="content"
+              className="materialize-textarea"
+              onChange={this.handleChange}
+            ></textarea>
           </div>
           <div className="input-field">
-            <button className="btn pink lighten-1 z-depth-0">Create Project</button>
+            <button className="btn pink lighten-1 z-depth-0">
+              Create Project
+            </button>
           </div>
         </form>
       </div>
@@ -37,4 +45,10 @@ export class CreateProject extends Component {
   }
 }
 
-export default CreateProject;
+const mapDispatchToProps= (dispatch)=>{
+  return{
+    createProject: (project)=>dispatch(createProject(project))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CreateProject);
